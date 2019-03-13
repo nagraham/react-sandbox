6d95626cdd61d8c13098e536d347378f87aae632
Alex Graham <nagraham00@hotmail.com>
2019-03-13 08:07:01 -0700

    Add App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-// import "./App.css";
+import "./App.css";
 
 class App extends React.Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1>Hello, World!</h1>
+        <p>This is awesome!</p>
       </div>
     );
   }
